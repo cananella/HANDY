57734a47b0c118a62df2bbe250c579dcbca9fc0f
cananella <mango7460@naver.com>
2025-10-02 23:46:52 +0900

    vision_controller

--- src/vision_controller/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export THIS_DIR=$(dirname "$(realpath "$0")")
-export PYTHONPATH="$THIS_DIR/openpose_wrapper:$PYTHONPATH"
-export LD_LIBRARY_PATH="$THIS_DIR/openpose_wrapper:$LD_LIBRARY_PATH"
-python3 pose_joint_estimator_openpose.py
